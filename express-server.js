03b98634c641873d9a7e862073a4c4c74f6caf3a
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-08 22:20:52 -0500

    implementing dummy handle for post requests

--- express-server.js
+++ express-server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
+// setting ejs as the view engine
 app.set('view engine', 'ejs');
+
+// using bodyparser to handle request body from buffer
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -41,4 +46,19 @@ app.get('/urls/:shortURL', (req,res)=>{
   res.render('urls_show', templateVars);
 });
 
+const generateRandomString = () => {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+// handling form POST
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
